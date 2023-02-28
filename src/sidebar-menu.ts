70f4159d4166abe383591232c00f49784b6c5a8a
lschirmbrand <luca.schirmbrand@hotmail.com>
2023-02-28 16:51:06 +0100

    feature -> general improvements, collapsing and more

--- src/sidebar-menu.ts
+++ src/sidebar-menu.ts
@@ -2,33 +2,46 @@ import { BaseCustomWebComponentConstructorAppendLazyReady, css, html, TypedEvent
 
 export class SidebarMenu extends BaseCustomWebComponentConstructorAppendLazyReady {
     static readonly style = css`
+        * {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-size: 1.1rem;
+            color: white;
+        }
+
         nav.sidebar {
-            height: 100%;
-            min-width: 80px;
-            max-width: 250px;
+            height: calc(100% - 40px);
             background-color: #21333D;
             display: flex;
             flex-direction: column;
+            box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
         }
 
         div.sidebar-cell {
             display: grid;
-            grid-template-columns: 80px 1fr 20px;
-            padding-right: 10px;
-            height: 80px;
+            padding: 10px;
             align-items: center;
-            justify-content: left;
             cursor: pointer;
             color: white;
+            grid-template-columns: 1fr 20px;
+            gap: 10px;
+            white-space: nowrap;
+            height: 100px;
+        }
+
+        div.sidebar-cell.has-icon {
+            grid-template-columns: 80px 1fr 20px;
+        }
+
+        nav.sidebar.compact>div.sidebar-cell {
+            grid-template-columns: 80px 20px;
+            gap: 0px;
+        }
+        nav.sidebar.compact>div.sidebar-cell>div.sidebar-cell-text {
+            display: none;
         }
 
         div.sidebar-cell:hover {
             background-color: #2E4A5A;
-        }
-
-        div.sidebar-cell-icon {
-            width: 80px;
-            height: 80px;
         }
 
         div.sidebar-cell-text {
@@ -38,19 +51,42 @@ export class SidebarMenu extends BaseCustomWebComponentConstructorAppendLazyRead
         div#subMenu {
             position: absolute;
             background-color: #21333D;
-            // min-width: 230px;
             border-left: 3px solid #2E4A5A;
             flex-direction: column;
             visibility: hidden;
+            box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
         }
 
         div#subMenu.sidebar-menu-visible {
             visibility: visible;
         }
+
+        #collapse {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 30px;
+            background-color: #21333D;
+            padding: 5px;
+        }
+
+        #collapse.turned {
+            rotate: 180deg;
+        }
+
+        #collapse > * {
+            height: 100%;
+        }
+
+        #icon {
+            background-color: gray;
+            border-radius: 10px;
+        }
     `;
 
     static readonly template = html`
-        <nav id=sidebar class="sidebar"></nav>
+        <nav id="sidebar" class="sidebar"></nav>
+        <span id="collapse"><span>
     `;
 
     static readonly properties = {
@@ -61,42 +97,45 @@ export class SidebarMenu extends BaseCustomWebComponentConstructorAppendLazyRead
 
     public menuItems: SideBarMenuChildWithIcon[] = [];
     private sidebar: HTMLDivElement;
+    private collapseElem: HTMLElement;
+
+
+    private isCompact: boolean = false;
 
     constructor() {
         super();
         this._restoreCachedInititalValues();
-        this.collapseMenu = this.collapseMenu.bind(this);
-    }
-
-    connectedCallback() {
-        window.addEventListener("click", this.collapseMenu)
-    }
-
-    disconnectedCallback() {
-        window.removeEventListener("click", this.collapseMenu)
+        this.collapseAllMenu = this.collapseAllMenu.bind(this);
     }
 
     ready() {
         this._parseAttributesToProperties();
         this.sidebar = this._getDomElement<HTMLDivElement>("sidebar");
         this.buildMenu(this.menuItems, this.sidebar);
+
+        this.collapseElem = this._getDomElement<HTMLElement>("collapse")
+        this.collapseElem.appendChild(SidebarMenu.expanderIcon.content.cloneNode(true));
+        this.collapseElem.onpointerdown = () => this.switchBetweenSidebarCompactness();
     }
 
-    buildMenu(menuItems: SideBarMenuChildWithIcon[], host: HTMLElement) {
+    private buildMenu(menuItems: SideBarMenuChildWithIcon[], host: HTMLElement) {
         for (let item of menuItems) {
-            host.appendChild(this.buildItem(item, 0))
+            host.appendChild(this.buildItem(item, 1))
         }
     }
 
-    private collapseMenu(event: MouseEvent) {
+    private collapseAllMenu(event: MouseEvent) {
         let elem = <HTMLElement>event.composedPath()[0];
-        if (this.isSubNodeOf(elem, this.sidebar)) {
+        if (!this.isSubNodeOf(elem, this.sidebar)) {
+            this.collapseMenuTillDepth(0);
+        }
+    }
 
-        } else {
-            let elements = this._getDomElements<HTMLElement>("div#subMenu");
-            for (let el of elements) {
+    private collapseMenuTillDepth(depth: number) {
+        let elements = this._getDomElements<HTMLElement>("div#subMenu");
+        for (let el of elements) {
+            if (!el["$depth"] || el["$depth"] > depth)
                 el.classList.remove("sidebar-menu-visible");
-            }
         }
     }
 
@@ -109,38 +148,54 @@ export class SidebarMenu extends BaseCustomWebComponentConstructorAppendLazyRead
         return false;
     }
 
-
     private buildItem(item: SideBarMenuChildWithIcon, depth: number): HTMLElement {
-        let elem = item.children?.length < 1
-            ? (<DocumentFragment>SidebarMenu.sidebarMenuItem.content.cloneNode(true)).children[0] as HTMLElement
-            : (<DocumentFragment>SidebarMenu.sidebarMenuItemWithChildren.content.cloneNode(true)).children[0] as HTMLElement;
+        let elem: HTMLElement;
+        if (depth == 1) {
+            elem = !item.children || item.children.length < 1
+                ? (<DocumentFragment>SidebarMenu.sidebarTopItem.content.cloneNode(true)).children[0] as HTMLElement
+                : (<DocumentFragment>SidebarMenu.sidebarTopItemWithChildren.content.cloneNode(true)).children[0] as HTMLElement;
+        } else {
+            elem = !item.children || item.children?.length < 1
+                ? (<DocumentFragment>SidebarMenu.sidebarMenuItem.content.cloneNode(true)).children[0] as HTMLElement
+                : (<DocumentFragment>SidebarMenu.sidebarMenuItemWithChildren.content.cloneNode(true)).children[0] as HTMLElement;
+        }
+
         let content = elem.querySelector("#content");
         let icon = elem.querySelector("#icon");
 
         content.innerHTML = item.displayName;
-        icon.innerHTML = item.iconIsHtml ? item.icon : "";
+
+        if (icon) icon.innerHTML = item.iconIsHtml ? item.icon : "";
 
         elem["$data"] = item;
-
-        elem.onmousedown = (e) => { this.menuItemPressed(elem, e) }
 
         if (item.children && item.children.length > 0) {
             elem.appendChild((<HTMLElement>SidebarMenu.expanderIcon.content.cloneNode(true)).children[0]);
             let frac = document.createDocumentFragment();
             for (let it of item.children) {
-                frac.appendChild(this.buildItem(it as SideBarMenuChildWithIcon, depth++));
+                frac.appendChild(this.buildItem(it as SideBarMenuChildWithIcon, depth + 1));
             }
             let subMenu = elem.querySelector("div#subMenu") as HTMLElement;
+            subMenu["$depth"] = depth;
             subMenu.appendChild(frac);
+        }
+
+        elem.onpointerdown = (e) => {
+            if (!e.defaultPrevented)
+                this.menuItemPressed(elem, /*e*/);
+            e.preventDefault();
         }
 
         return elem;
     }
 
-    private menuItemPressed(element: HTMLElement, event: MouseEvent) {
+    private menuItemPressed(element: HTMLElement, /*event: MouseEvent*/) {
         if (!element["$data"].children || element["$data"].children.length < 1) {
             this.sidebarItemPressed.emit(element["$data"]);
+            this.collapseMenuTillDepth(0);
         } else {
+            // Close all SubMenus, where depth lt higher new depth
+            this.collapseMenuTillDepth(element.parentElement["$depth"] ?? 0);
             // Expand
             let rect = element.getBoundingClientRect();
             let subMenu = element.querySelector("div#subMenu") as HTMLElement;
@@ -150,16 +205,40 @@ export class SidebarMenu extends BaseCustomWebComponentConstructorAppendLazyRead
         }
     }
 
-    static readonly sidebarMenuItem = html`
-        <div class="sidebar-cell" style="height: 80px;">
+    private switchBetweenSidebarCompactness() {
+        this.isCompact = !this.isCompact;
+        if (this.isCompact) {
+            this.sidebar.classList.add("compact");
+            this.collapseElem.classList.add("turned");
+        } else {
+            this.sidebar.classList.remove("compact");
+            this.collapseElem.classList.remove("turned");
+        }
+    }
+
+    static readonly sidebarTopItem = html`
+        <div class="sidebar-cell has-icon">
             <div class="sidebar-cell-icon" id="icon"></div>
             <div class="sidebar-cell-text" id="content"></div>
         </div>
     `;
 
-    static readonly sidebarMenuItemWithChildren = html`
-        <div class="sidebar-cell" style="height: 80px;">
+    static readonly sidebarTopItemWithChildren = html`
+        <div class="sidebar-cell has-icon">
             <div class="sidebar-cell-icon" id="icon"></div>
+            <div class="sidebar-cell-text" id="content"></div>
+            <div id="subMenu"></div>
+        </div>
+    `;
+
+    static readonly sidebarMenuItem = html`
+        <div class="sidebar-cell">
+            <div class="sidebar-cell-text" id="content"></div>
+        </div>
+    `;
+
+    static readonly sidebarMenuItemWithChildren = html`
+        <div class="sidebar-cell">
             <div class="sidebar-cell-text" id="content"></div>
             <div id="subMenu"></div>
         </div>
@@ -171,6 +250,14 @@ export class SidebarMenu extends BaseCustomWebComponentConstructorAppendLazyRead
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
             <g id="SVGRepo_iconCarrier"> <path d="M9 6L15 12L9 18" stroke="#FFFFFF" stroke-width="2"></path></g>
         </svg>
-    `
+    `;
+
+    connectedCallback() {
+        window.addEventListener("click", this.collapseAllMenu)
+    }
+
+    disconnectedCallback() {
+        window.removeEventListener("click", this.collapseAllMenu)
+    }
 }
 customElements.define("sidebar-menu", SidebarMenu);
